b09a6ec1bca433a0dd2c5743b1391ca7783eb896
Mohin-Uddin-Mishrat <mmishrat90@gmail.com>
2024-02-13 21:21:17 +0600

    csrf_issue

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,6 +20,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = ['https://library-system-kecv.onrender.com','https://*.127.0.0.1']
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
